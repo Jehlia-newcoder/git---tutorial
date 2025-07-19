380e375fcbead38eae47fe4c557a7f534aceafff
Jehlia Seco <jehliacco@gmail.com>
2025-07-19 16:24:09 +0800

    The other one

--- change.java
+++ change.java
@@ -3,4 +3,8 @@ public class change {
     String jeez = "This is a change for git learning and experimenting purposes";
     int one = 1;
     int two = 2;
+
+    double onne = 1.0;
+    doublw twoo = 2.2;
+    
 }
